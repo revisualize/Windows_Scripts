c893ab64431f71c0c8d5fea53faf0b271601b9b1
Joseph <github.com@revisualized.com>
2017-05-31 11:30:46 -0700

    Update Get-ADUserLastLogon.ps1

--- Get-ADUserLastLogon.ps1
+++ Get-ADUserLastLogon.ps1
@@ -2,11 +2,12 @@ Get-ADUser -Filter {Enabled -eq $true} -Properties * | `
      Select-Object GivenName, `
                    Surname, `
                    SamAccountName, `
+                   Title, `
                    Department, `
                    @{Name="LastLogon TimeStamp";Expression={[datetime]::FromFileTime($_.'lastLogonTimeStamp')}}, `
                    @{Name="Last Logon";Expression={[datetime]::FromFileTime($_.'lastLogon')}}, `
                    PasswordExpired, `
-                   PasswordLastSet, `
+                   whenCreated, `
                    @{Name='Manager';Expression={(Get-ADUser $_.Manager).GivenName + " " + (Get-ADUser $_.Manager).Surname}} | `
-     Sort-Object "Last Logon" | `
+     Sort-Object @{Expression='Department'; Ascending=$true } , @{Expression='SamAccountName'; Ascending=$true } | `
      Format-Table
