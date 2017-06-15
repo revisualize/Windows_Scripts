b6f1fb97f87503c1d67546ebb0b27dbdddfcd291
Joseph <github.com@revisualized.com>
2017-06-15 12:34:47 -0700

    Update Get_all_ADUsers_with_GivenName_SurName_Sort_Department_GivenName.ps1

--- Get_all_ADUsers_with_GivenName_SurName_Sort_Department_GivenName.ps1
+++ Get_all_ADUsers_with_GivenName_SurName_Sort_Department_GivenName.ps1
@@ -1,1 +1,10 @@
-Get-ADUser -Filter * -Properties * | Select-Object Department,GivenName,SurName,Title,@{Name='Manager';Expression={(Get-ADUser $_.Manager).GivenName + " " + (Get-ADUser $_.Manager).Surname}},Enabled | Where-Object {($_.GivenName -ne $null) -and ($_.SurName -ne $null)} | Sort-Object @{Expression='Department'; Ascending=$true } , @{Expression='GivenName'; Ascending=$true } | Format-Table
+Get-ADUser -Filter * -Properties Department, GivenName, SurName, Title, Manager, Enabled | `
+    Select-Object Department, `
+                  GivenName, `
+                  SurName, `
+                  Title, `
+                  @{Name='Manager';Expression={(Get-ADUser $_.Manager).GivenName + " " + (Get-ADUser $_.Manager).Surname}}, `
+                  Enabled | `
+         Where-Object {($_.GivenName -ne $null) -and ($_.SurName -ne $null)} | `
+         Sort-Object @{Expression='Department'; Ascending=$true } , @{Expression='GivenName'; Ascending=$true } | `
+         Format-Table
