b38eb4e6c52b3ab121652d72e20645b90c86eeff
Joseph <github.com@revisualized.com>
2018-07-24 12:29:09 -0700

    Update Create-ScheduledTaskUnsplashWallpaper.ps1

--- Create-ScheduledTaskUnsplashWallpaper.ps1
+++ Create-ScheduledTaskUnsplashWallpaper.ps1
@@ -1,24 +1,34 @@
 Function Create-ScheduledTaskUnsplashWallpaper {
 
+
+
+    $TaskWorkingDirectory = "D:\Unsplash_Walls\"
+
+    $TaskScript = "C:\Users\joseph\OneDrive\Documents\PowerShell\Save-UnsplashPicture.ps1"
+
+
+
+
+
     $RandomNumber = Get-Random -Minimum 1234 -Maximum 9999
 
     $TaskName = "1 hour Unsplash Wallpaper $($RandomNumber)"
 
     $TaskDescription = "One hour timer to Get a new random wallpaper from Save-UnsplashPicture.ps1"
 
-    $TaskExcecute = "powershell.exe"
-
-    $TaskScript = ".\Save-UnsplashPicture.ps1"
+    $TaskExcecute = "Powershell.exe"
 
     $TaskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 00:10:00 -RunOnlyIfNetworkAvailable -StartWhenAvailable -DisallowStartOnRemoteAppSession
 
-    $TaskWorkingDirectory = "D:\Unsplash_Walls\"
-
+    $TaskDeleteFiles = 'Get-ChildItem -Path ' 
+    $TaskDeleteFiles += $TaskWorkingDirectory
+    $TaskDeleteFiles += ' -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt \$((Get-Date).AddDays(-5)) } | Remove-Item -Force'
+    
     $TaskAction = @()
     $TaskAction += New-ScheduledTaskAction -Execute $TaskExcecute -Argument $TaskScript -WorkingDirectory $TaskWorkingDirectory
-    $TaskAction += New-ScheduledTaskAction -Execute $TaskExcecute -Argument 'Get-ChildItem -Path "D:\Unsplash_Walls\" -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $((Get-Date).AddDays(-5)) } | Remove-Item -Force' -WorkingDirectory $TaskWorkingDirectory
+    $TaskAction += New-ScheduledTaskAction -Execute $TaskExcecute -Argument $TaskDeleteFiles -WorkingDirectory $TaskWorkingDirectory
 
-
+    $TaskAction
     $TaskTrigger = New-ScheduledTaskTrigger -Daily -At 1:00:00 -ThrottleLimit 30
 
     Register-ScheduledTask -TaskName $TaskName -Action $TaskAction -Settings $TaskSettings -Trigger $TaskTrigger -Description $TaskDescription
