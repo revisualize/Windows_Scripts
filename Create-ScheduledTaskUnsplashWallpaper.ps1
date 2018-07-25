fd986425f9edfc33d057dfab09b5cf5550847abb
Joseph <github.com@revisualized.com>
2018-07-24 18:38:57 -0700

    Update Create-ScheduledTaskUnsplashWallpaper.ps1

--- Create-ScheduledTaskUnsplashWallpaper.ps1
+++ Create-ScheduledTaskUnsplashWallpaper.ps1
@@ -16,7 +16,11 @@ Function Create-ScheduledTaskUnsplashWallpaper {
 
     $TaskDescription = "One hour timer to Get a new random wallpaper from Save-UnsplashPicture.ps1"
 
-    $TaskExcecute = "Powershell.exe"
+    $TaskRepetition = (New-TimeSpan -Minutes 60)
+    $TaskDuration = ([timeSpan]::maxvalue)
+
+    $TaskExecute = $pshome
+    $TaskExecute += "\powershell.exe"
 
     $TaskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 00:10:00 -RunOnlyIfNetworkAvailable -StartWhenAvailable -DisallowStartOnRemoteAppSession
 
@@ -25,11 +29,15 @@ Function Create-ScheduledTaskUnsplashWallpaper {
     $TaskDeleteFiles += ' -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $((Get-Date).AddDays(-5)) } | Remove-Item -Force'
     
     $TaskAction = @()
-    $TaskAction += New-ScheduledTaskAction -Execute $TaskExcecute -Argument $TaskScript -WorkingDirectory $TaskWorkingDirectory
-    $TaskAction += New-ScheduledTaskAction -Execute $TaskExcecute -Argument $TaskDeleteFiles -WorkingDirectory $TaskWorkingDirectory
+    $TaskAction += New-ScheduledTaskAction -Execute $TaskExecute -Argument $TaskScript -WorkingDirectory $TaskWorkingDirectory
+    $TaskAction += New-ScheduledTaskAction -Execute $TaskExecute -Argument $TaskDeleteFiles -WorkingDirectory $TaskWorkingDirectory
 
-    $TaskTrigger = New-ScheduledTaskTrigger -Daily -At 1:00:00 -ThrottleLimit 30
+    $TaskRepetition = (New-TimeSpan -Minutes 60)
+    $TaskDuration = (New-TimeSpan -Days 20000)    #54.8 years 
 
+    $TaskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval $TaskRepetition -RepetitionDuration $TaskDuration
+    #New-ScheduledTaskTrigger -Daily -At 1:00:00 -ThrottleLimit 30
+    
     Register-ScheduledTask -TaskName $TaskName -Action $TaskAction -Settings $TaskSettings -Trigger $TaskTrigger -Description $TaskDescription
 }
 Create-ScheduledTaskUnsplashWallpaper
