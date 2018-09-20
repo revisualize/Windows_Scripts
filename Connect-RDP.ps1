f6e591234702e795e60961a22a699fcb6b372420
Joseph <github.com@revisualized.com>
2018-09-19 19:50:42 -0700

    Create Connect-RDP.ps1

---
+++ Connect-RDP.ps1
@@ -0,0 +1,29 @@
+Function Connect-RDP {
+    Param (
+        [Parameter(Mandatory=$True,
+                   HelpMessage="Enter a ComputerName"
+                   )] 
+                   [string]
+                   [Alias("cn","comp")]
+                   $ComputerName
+    )
+
+    Function Get-DnsHostName {
+
+        Param (
+            [Parameter(Mandatory=$True,
+                       HelpMessage="Enter a ComputerName"
+                       )] 
+                       [string]
+                       [Alias("cn","comp","ComputerName")]
+                       $Host
+        )
+
+        Return [System.Net.Dns]::GetHostByAddress( [System.Net.Dns]::GetHostByName($ComputerName).AddressList.IpAddressToString ).HostName
+    }
+    
+    $ServerHostName = Get-DnsHostName -Host $ComputerName
+    
+    Start-Process "$env:windir\system32\mstsc.exe" -ArgumentList "/v:$ServerHostName"
+    
+}
